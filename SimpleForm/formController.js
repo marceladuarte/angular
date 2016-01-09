70676ba814205b1d58927790df2fcf8544df7fce
marceladuarte <marcela.pedrinid@gmail.com>
2016-01-09 20:36:03 -0200

    Basic CRUD operations

--- SimpleForm/formController.js
+++ SimpleForm/formController.js
@@ -1,21 +1,66 @@
 var app = angular.module("simpleForm", []);
 app.controller("formController", function($scope){
-      $scope.user = {
+      $scope.customer = {
+                      id: 0,
                       name: "",
                       lastname: "",
                       age: "",
                       email: ""
                     };
-      $scope.users = [];
+      $scope.customers = [];
+      $scope.id = 0;
+      $scope.currentId = 0;
 
       $scope.save = function() {
-        $scope.user.name = $scope.name;
-        $scope.user.lastname = $scope.lastname;
-        $scope.user.age = $scope.age;
-        $scope.user.email = $scope.email;
-        var list  = $scope.users.concat( $scope.user );
-        $scope.users = list;
+        if($scope.currentId == 0){
+          $scope.customer.id = $scope.id + 1;
+          $scope.customer.name = $scope.name;
+          $scope.customer.lastname = $scope.lastname;
+          $scope.customer.age = $scope.age;
+          $scope.customer.email = $scope.email;
+
+          $scope.id = $scope.customer.id;
+
+          var list  = $scope.customers.concat( $scope.customer );
+          $scope.customers = list;
+
+        }else{
+          var index = $scope.findIndexById($scope.currentId);
+          $scope.remove(index);
+          $scope.currentId = 0;
+          $scope.save();
+        }
         $scope.cleanForm();
+
+      };
+
+      $scope.edit = function(id){
+        var index = $scope.findIndexById(id);
+        $scope.name = $scope.customers[index].name;
+        $scope.lastname = $scope.customers[index].lastname;
+        $scope.age = $scope.customers[index].age;
+        $scope.email = $scope.customers[index].email;
+        $scope.currentId = id;
+
+      };
+
+      $scope.confirmRemove = function(id){
+        if (confirm('Are you sure you want to delete this customer?')) {
+          var index = $scope.findIndexById(id);
+          $scope.remove(index);
+        }
+      };
+
+      $scope.remove = function(index){
+        $scope.customers.splice(index, 1);
+      };
+
+      $scope.findIndexById = function(id){
+        for (var index = 0; index < $scope.customers.length; index++){
+          if($scope.customers[index].id == id){
+            return index;
+          }
+        }
       };
 
       $scope.cleanForm = function(){
@@ -23,12 +68,14 @@ app.controller("formController", function($scope){
         $scope.lastname = "";
         $scope.age = "";
         $scope.email = "";
-        $scope.user = {
+        $scope.customer = {
+                        id: 0,
                         name: "",
                         lastname: "",
                         age: "",
                         email: ""
                       };
+        $scope.currentId = 0;
       };
 
 });
