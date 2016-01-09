cdebe7a8bf82635430267219df50b04641b753a3
marceladuarte <marcela.pedrinid@gmail.com>
2016-01-08 23:09:33 -0200

    Add Bootstrap libs

--- SimpleForm/formController.js
+++ SimpleForm/formController.js
@@ -1,11 +1,18 @@
 var app = angular.module("simpleForm", []);
 app.controller("formController", function($scope){
-      $scope.user = {name: "", lastname: ""};
+      $scope.user = {
+                      name: "",
+                      lastname: "",
+                      age: "",
+                      email: ""
+                    };
       $scope.users = [];
 
       $scope.save = function() {
         $scope.user.name = $scope.name;
         $scope.user.lastname = $scope.lastname;
+        $scope.user.age = $scope.age;
+        $scope.user.email = $scope.email;
         var list  = $scope.users.concat( $scope.user );
         $scope.users = list;
         $scope.cleanForm();
@@ -16,7 +23,12 @@ app.controller("formController", function($scope){
         $scope.lastname = "";
         $scope.age = "";
         $scope.email = "";
-        $scope.user = {name: "", lastname: ""};
+        $scope.user = {
+                        name: "",
+                        lastname: "",
+                        age: "",
+                        email: ""
+                      };
       };
 
 });
